2ee0703caea39b0ac0f3a5eb55bb42b9d3a89311
GANESH-6197 <25331a05d2@mvgrce.edu.in>
2026-04-07 17:09:53 +0530

    hellouser.c

--- Week-1/hellouser.c
+++ Week-1/hellouser.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 int main() {
 char username [6];
-printf("Enter your name");
+printf("Enter your name: ");
 scanf("%s",username);
-printf("hello%s!!!",username);
+printf("hello %s!!!",username);
+printf("\n\n           -25331A05D2");
 return 0;
 }
