7ea78e4cb7b0cbd9ec1cf92f0cb65dac4a9f9e9e
GANESH-6197 <25331a05d2@mvgrce.edu.in>
2026-04-08 09:28:18 +0530

    fibonacci.c

--- Week-4/fibonacci.c
+++ Week-4/fibonacci.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main(){
 int a=0,b=1,c,i,n;
-printf("enter number of terms");
+printf("enter number of terms: ");
 scanf("%d",&n);
-printf("fibonacci series");
+printf("fibonacci series: ");
 for(i=1;i<=n;i++)
 {
 printf("%d",a);
@@ -11,6 +11,6 @@ c=a+b;
 a=b;
 b=c;
 }
+printf("\n\n             -25331A05D2");
 return 0;
 }
-
